88a3c3bec54b7e12903d604771cf0f77bf585cd2
Rasmus-asberg-nti-johanneberg <rasmus.asberg@elev.ga.ntig.se>
2020-02-26 15:04:53 +0100

    Struktur och burger meny
    Har lyckats få koll på min grid struktur och hur den allmänt ska se ut samt har jobbat en del på burger menyn som nästan blev klar.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMeny() {
+
+    document.querySelector(".navlinks").classList.toggle("navactive")
+    document.querySelector(".burger").classList.toggle("toggle")
+    
+}
+
+document.querySelector(".burger").addEventListener("click", toggleMeny)
